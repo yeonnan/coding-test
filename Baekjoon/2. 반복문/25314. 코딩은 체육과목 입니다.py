11babf138e7e9c2cf4505447db07a50125d23ca3
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-09-26 02:17:12 +0900

    백준 반복문

--- Baekjoon/2. 반복문/25314. 코딩은 체육과목 입니다.py
+++ Baekjoon/2. 반복문/25314. 코딩은 체육과목 입니다.py
@@ -1,11 +1,1 @@
-# long int = 4
-# long long int = 8
-# long long long int = 12
-# long long long long int = 16
-
-N = int(input())
-
-for _ in range(N//4):
-    # end=' '는 출력 후 줄바꿈 대신 공백을 추가함
-    # 출력 후 기본적으로 줄바꿈을 하지 않고, 자동 줄바꿈 대신 빈 문자열을 출력하게 하는 설정
-    print('long', end = '')
+print(int(input())//4*'long ' + 'int')
