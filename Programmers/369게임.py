c40ba5ed34104e3e4e8b61c234d426269fa77e33
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-10-22 22:10:19 +0900

    369게임

---
+++ Programmers/369게임.py
@@ -0,0 +1,10 @@
+def solution(order):
+    answer = 0
+
+    for i in str(order):
+        if i=='3' or i=='6' or i=='9':
+            answer += 1
+    return answer
+
+order = int(input())
+print(solution(order))
